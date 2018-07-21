2ff32ec25c13cda2701ccbec74ddd0d624f90389
Chad <chad3281@gmail.com>
2018-07-20 21:48:29 -0400

    Cleanup

--- js/layout.js
+++ js/layout.js
@@ -11,41 +11,6 @@ function loadNavBar(callback)
             callback();
         }
     });
-    /*tags[0].innerHTML = 
-    '<div class="navbar navbar-inverse">' +
-        '<div class="navbar-header">' +
-            '<a class="navbar-brand" id="nav-name">Chad Jakoby</a>' +
-            '<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">' +
-                '<span class="icon-bar"></span>' +
-                '<span class="icon-bar"></span>' +
-                '<span class="icon-bar"></span>' +
-            '</button>' +
-            '<a class="navbar-brand show-mobile" href="https://github.com/jakobyc" style="float: right; color: #83168e;">' +
-                '<i class="fa fa-lg fa-github" aria-hidden="true"></i>' +
-            '</a>' +
-            '<a class="navbar-brand show-mobile" href="https://www.linkedin.com/in/chad-jakoby-084599bb" style="float: right; color: #3570b3;">' +
-                '<i class="fa fa-lg fa-linkedin" aria-hidden="true"></i>' +
-            '</a>' +
-        '</div>' +
-        '<div class="navbar-collapse collapse">' +
-            '<ul class="nav navbar-nav">' +
-                '<li><a href="index.html" id="navHome">Home</a></li>' +
-                '<li><a href="projects.html" id="navProjects">Projects</a></li>' +
-            '</ul>' +
-            '<ul class="nav navbar-nav navbar-right">' +
-                '<li>' +
-                    '<a class="theme-social-git hide-mobile" href="https://github.com/jakobyc" target="_blank">' +
-                        '<i class="fa fa-lg fa-github" aria-hidden="true"></i>' +
-                    '</a>' +
-                '</li>' +
-                '<li>' +
-                    '<a class="theme-social-linkedin hide-mobile" href="https://www.linkedin.com/in/chad-jakoby-084599bb" target="_blank">' +
-                        '<i class="fa fa-lg fa-linkedin" aria-hidden="true"></i>' +
-                    '</a>' +
-                '</li>' +
-            '</ul>' +
-        '</div>' +
-    '</div>';*/
 }
 
 // Search for a <footer> tag and populate it's inner HTML. 
@@ -54,15 +19,8 @@ function loadFooter()
     var tags = document.getElementsByTagName('footer');
     $.get('layout/footer.html', function(data)
     {
-        $(tags[0]).html(data);
-        //tags[0].innerHTML = data;
+        tags[0].innerHTML = data;
     });
-    /*tags[0].innerHTML =
-    '<div id="footer">' +
-        '<div class="container">' +
-            '<p style="float: right">&copy; Chad Jakoby</p>' +
-        '</div>' +
-    '</div>';*/
 }
 
 function setActive(navLink)
