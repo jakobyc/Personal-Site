55b41c178f1ed6e452c0e7f71e5e40fa08fef648
Chad <chad3281@gmail.com>
2019-03-11 18:38:42 -0400

    IE Compatability fixes. RIP ES6 syntax.

--- js/github.js
+++ js/github.js
@@ -1,88 +1,39 @@
 const baseUrl = 'https://api.github.com/';
 
-class GitHub
+function GitHub() { }
+
+// Return repo object from API:
+GitHub.prototype.getRepo = function(user, repo, callback)
 {
-    constructor()
-    {
-    }
-
-    // Return repo object from API:
-    getRepo(user, repo, callback)
-    {
-        $.getJSON(baseUrl + "repos/" + user + "/" + repo, function (data)
+    $.getJSON(baseUrl + "repos/" + user + "/" + repo, function (data)
         {
             callback(data);
         });
-    }
+}
 
-    // Return all repos for a user:
-    getRepos(user, callback)
+// Return all repos for a user:
+GitHub.prototype.getRepos = function(user, callback)
+{
+    var cache = sessionStorage.getItem('repos');
+    if (!cache)
     {
-        var cache = sessionStorage.getItem('repos');
-        if (!cache)
+        $.getJSON(baseUrl + "users/" + user + "/repos", function (data)
         {
-            $.getJSON(baseUrl + "users/" + user + "/repos", function (data)
-            {
-                sessionStorage.setItem('repos', JSON.stringify(data));
-                callback(data);
-            });
-        }
-        else
-        {
-            cache = JSON.parse(cache);
-            callback(cache);
-        }
-    }
-
-    getIssues(user, repo, callback)
-    {
-        $.getJSON(baseUrl + "repos/" + user + "/" + repo + "/issues", function (data)
-        {
+            sessionStorage.setItem('repos', JSON.stringify(data));
             callback(data);
         });
+    }
+    else
+    {
+        cache = JSON.parse(cache);
+        callback(cache);
     }
 }
 
-
-// element = element to append the template to.
-/*function showIssues(user, repo, element)
+GitHub.prototype.getIssues = function(user, repo, callback)
 {
-    const templateString =
-        '<div class="well well-sm">' +
-            '<label>@state - @label</label>' +
-            '<label style="float: right;">@date</label>' +
-            '<h3>(#@issueNumber) @title</h3>' +
-            '<p>@body</p>' +
-            '<a class="btn theme-btn-git" target="_blank" href="@url">View on GitHub</a>' +
-        '</div>';
-
-    const emptyString =
-        '<div class=well well-sm>' +
-            '<h3>No open issues exist for this repository.</h3>' +
-        '</div>';
-
-    getIssues(user, repo, function(data)
+    $.getJSON(baseUrl + "repos/" + user + "/" + repo + "/issues", function (data)
     {
-        if (data.length > 0) {
-            $.each(data, function ()
-            {
-                var template = templateString;
-                template = template.replace('@state', this.state);
-                template = template.replace('@title', this.title);
-                template = template.replace('@body', this.body);
-                template = template.replace('@issueNumber', this.number)
-                template = template.replace('@url', this.html_url);
-                template = template.replace('@date', this.created_at);
-                if (this.labels != null && this.labels.length > 0)
-                {
-                    template = template.replace('@label', this.labels[0].name);
-                }
-                $(element).append(template);
-            });
-        }
-        else
-        {
-            $(element).append(emptyString);
-        }
+        callback(data);
     });
-}*/
+}
--- js/layout.js
+++ js/layout.js
@@ -1,66 +1,38 @@
-class Layout
+function Layout() { }
+
+// Search for a <footer> tag and populate it's inner HTML. 
+Layout.prototype.loadFooter = function()
 {
-    // Search for a <footer> tag and populate it's inner HTML. 
-    loadFooter()
-    {
-        var tags = document.getElementsByTagName('footer');
+    var tags = document.getElementsByTagName('footer');
         $.get('layout/footer.html', function(data)
         {
             tags[0].innerHTML = data;
         });
-    }
+}
 
-        // Search for a <nav> tag and populate it's inner HTML. 
-    loadNavBar(callback)
+// Search for a <nav> tag and populate it's inner HTML. 
+Layout.prototype.loadNavBar = function(callback)
+{
+    var tags = document.getElementsByTagName('nav');
+    $.get('layout/navbar.html', function(data)
     {
-        var tags = document.getElementsByTagName('nav');
-        $.get('layout/navbar.html', function(data)
+        tags[0].innerHTML = data;
+
+        if (callback)
         {
-            tags[0].innerHTML = data;
+            callback();
+        }
+    });
+}
 
-            if (callback)
-            {
-                callback();
-            }
-        });
-    }
+// Callback is invoked after header is created
+Layout.prototype.load = function(callback)
+{
+    this.loadNavBar(callback);
+    this.loadFooter();
+}
 
-    setActive(navLink)
-    {
-        $(navLink).addClass("active");
-    }
-
-
-    // Callback is invoked after header is created
-    load(callback)
-    {
-        this.loadNavBar(callback);
-        this.loadFooter();
-    }
-
-    // Add listeners to sidenav to toggle displayed content.
-    enableSidenavClick()
-    {
-        $('.sidenav a').click(function () {
-            if (!$(this).hasClass('active-item')) {
-                // Get id of element to display:
-                var showId = $(this).attr('href');
-
-                // Remove all active-item classes:
-                $('.sidenav a').removeClass('active-item');
-
-                // Hide displayed content:
-                $('.sidenav-content-item').hide();
-                //$('.sidenav-content.active-item').hide();
-
-                // Show element and set sidenav item to active:
-                $(showId).show();
-                $(this).addClass('active-item');
-
-
-                // Prevent href from moving to the top of the page:
-                return false;
-            }
-        });
-    }
+Layout.prototype.setActive = function(navLink)
+{
+    $(navLink).addClass("active");
 }
